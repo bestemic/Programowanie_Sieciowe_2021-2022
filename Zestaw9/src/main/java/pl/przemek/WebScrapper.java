0f79b5ddc7c3266dbb84b04e0920256ea68ad407
bestemic <przemek.pawlik123@gmail.com>
2022-05-10 15:13:50 +0200

    Webscraper refactor

--- Zestaw9/src/main/java/pl/przemek/WebScrapper.java
+++ Zestaw9/src/main/java/pl/przemek/WebScrapper.java
@@ -11,20 +11,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WebScrapper {
+    private static final Pattern currencyNamePattern = Pattern.compile("^[A-Z][a-z]+( [A-Z][a-z]+)*$");
+    private static final Pattern exchangeRatePattern = Pattern.compile("^\\d+(\\.\\d)\\d*$");
+
     public static void main(String[] args) {
         final String url = "https://www.x-rates.com/table/?from=USD&amount=1";
 
         try {
             final Connection.Response response = getPage(url);
-
             if (response == null) {
                 System.exit(1);
             }
 
-            if (!parsePage(response)) {
+            final Elements rows = getRows(response);
+            if (rows == null) {
                 System.exit(1);
             }
 
+            if (!parseRows(rows)) {
+                System.exit(1);
+            }
         } catch (IOException e) {
             System.exit(1);
         }
@@ -33,7 +39,6 @@ public class WebScrapper {
     }
 
     public static Connection.Response getPage(String url) throws IOException {
-
         final Connection.Response response = Jsoup.connect(url).timeout(10000).execute();
         if (response.statusCode() != 200) {
             return null;
@@ -44,40 +49,34 @@ public class WebScrapper {
         if (!response.body().contains("US Dollar Exchange Rates Table")) {
             return null;
         }
-
         return response;
     }
 
-    public static boolean parsePage(Connection.Response response) throws IOException {
-
+    private static Elements getRows(Connection.Response response) throws IOException {
         final Document document = response.parse();
         final Elements tables = document.select(".tablesorter.ratesTable");
-
         if (tables.size() != 1) {
-            return false;
+            return null;
         }
 
         final Element table = tables.first();
-
         if (table.childrenSize() < 2) {
-            return false;
+            return null;
         }
 
         final Element tbody = table.child(1);
-
         if (tbody.childrenSize() < 1) {
-            return false;
+            return null;
         }
 
         final Elements rows = tbody.children();
-
         if (rows.size() < 1) {
-            return false;
+            return null;
         }
+        return rows;
+    }
 
-        final Pattern currencyNamePattern = Pattern.compile("^[A-Z][a-z]+( [A-Z][a-z]+)*$");
-        final Pattern exchangeRatePattern = Pattern.compile("^\\d+(\\.\\d)\\d*$");
-
+    public static boolean parseRows(Elements rows) throws IOException {
         for (Element row : rows) {
             if (row.childrenSize() != 3) {
                 return false;
@@ -85,23 +84,19 @@ public class WebScrapper {
 
             String currencyName = row.child(0).text();
             final Matcher currencyNameMatcher = currencyNamePattern.matcher(currencyName);
-
             if (!currencyNameMatcher.find()) {
                 return false;
             }
 
             currencyName = currencyName.replace(" ", "_");
-
             final String exchangeRate = row.child(1).text();
             final Matcher exchangeRateMatcher = exchangeRatePattern.matcher(exchangeRate);
-
             if (!exchangeRateMatcher.find()) {
                 return false;
             }
 
             System.out.println(currencyName + " " + exchangeRate);
         }
-
         return true;
     }
 }
