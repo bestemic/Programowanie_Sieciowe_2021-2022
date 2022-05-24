986179c7c30805837419fa6e5c3f44e59122948b
bestemic <przemek.pawlik123@gmail.com>
2022-05-24 20:59:43 +0200

    Artist finder v0

---
+++ Zestaw11/README.md
@@ -0,0 +1,29 @@
+# **Programowanie sieciowe** <br/> **Zestaw 11**
+<br>
+<div style="text-align: right"><b>Przemysław Pawlik</b></div>
+
+## **1.**
+Zapoznaj się z witryną Discogs oraz ze specyfikacją udostępnianego przez nią REST API.
+
+----------
+<br>
+
+## **2.**
+Spróbuj napisać program wyszukujący w bazie Discogs wszystkie albumy danego wykonawcy (np. Budki Suflera). Sprawdź czy to, co drukuje program zgadza się z tym, co można ręcznie wyszukać na witrynie Discogs.
+
+>Może się okazać, że Discogs odrzuca zapytania jeśli są one całkowicie anonimowe. Sprawdź wtedy w dokumentacji kim / czym trzeba być, aby uzyskać odpowiedź — zalogowanym użytkownikiem witryny, aplikacją napisaną przez zarejestrowanego dewelopera?
+
+----------
+<br>
+
+## **3.**
+Poszukaj innych dyskograficznych baz danych z publicznym API. Porównaj je z Discogs — czy ich API też są zgodne z zasadami REST? Jakie rodzaje obiektów są przetwarzane? Jak wyglądają URL-e odpowiadające tym obiektom, jakie operacje można na nich wykonywać? Kto ma prawo wywoływać poszczególne zdalne operacje?
+
+----------
+<br>
+
+## **4.**
+Czy powszechnie znane witryny takie jak Gmail, Facebook, Twitter, itd. udostępniają jakieś webAPI pozwalające aplikacjom wykonywać te same operacje, które potrafi wykonywać użytkownik korzystający z przeglądarki? Czy te webowe API są zaprojektowane zgodnie z zasadami architektury REST (czy są to tzw. RESTful APIs)?
+
+----------
+<br>
---
+++ Zestaw12/src/main/java/pl/przemek/ArtistFinder.java
@@ -0,0 +1,222 @@
+package pl.przemek;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.Collator;
+import java.util.*;
+
+public class ArtistFinder {
+    private static final String CONSUMER_KEY = "key";
+    private static final String CONSUMER_SECRET = "secret";
+    private static final String API_DATABASE = "https://api.discogs.com/database";
+    private static final String API = "https://api.discogs.com/artists";
+
+    private static String parentGroupName;
+
+    public static void main(String[] args) {
+        if (performOperation(args)) {
+            System.exit(0);
+        } else {
+            System.exit(1);
+        }
+    }
+
+    private static boolean performOperation(String[] args) {
+        final String groupIdentity = getGroupIdentity(args);
+        if (groupIdentity == null) {
+            return false;
+        }
+
+        try {
+            Map<String, String> artists = getArtists(groupIdentity);
+            if (artists == null) {
+                return false;
+            }
+
+            Map<String, Set<String>> groups = getGroups(artists);
+            if (groups == null) {
+                return false;
+            }
+
+            printGroups(groups);
+        } catch (IOException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    //TODO args checking
+    private static String getGroupIdentity(String[] args) {
+        if (args.length != 1) {
+            return null;
+        }
+        return args[0];
+    }
+
+    private static void printGroups(Map<String, Set<String>> groups) {
+        for (String groupName : groups.keySet()) {
+            if (groupName.equals(parentGroupName)) {
+                continue;
+            }
+            Set<String> members = groups.get(groupName);
+            if (members.size() == 1) {
+                continue;
+            }
+
+            System.out.print(groupName + ": ");
+            System.out.println(String.join(", ", members));
+        }
+    }
+
+    private static Map<String, String> getArtists(final String groupIdentity) throws IOException {
+        final String url = createUrl(groupIdentity);
+        if (url == null) {
+            return null;
+        }
+
+        String response = getResponse(url);
+        if (response == null) {
+            return null;
+        }
+
+        JSONObject jsonResponse = new JSONObject(response);
+        parentGroupName = getGroupNameFromResponse(jsonResponse);
+
+        return getArtistsFromResponse(jsonResponse);
+    }
+
+    private static String getGroupNameFromResponse(JSONObject response) {
+        try {
+            return String.valueOf(response.getString("name"));
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    private static String createUrl(String groupIdentity) throws IOException {
+        if (!groupIdentity.matches("[0-9]+")) {
+            String url = String.format("%s/search?type=artist&q=%s&per_page=1&key=%s&secret=%s", API_DATABASE, groupIdentity, CONSUMER_KEY, CONSUMER_SECRET);
+            String response = getResponse(url);
+            if (response == null) {
+                return null;
+            }
+            JSONObject jsonResponse = new JSONObject(response);
+            groupIdentity = getGroupIdFromResponse(jsonResponse);
+            if (groupIdentity == null) {
+                return null;
+            }
+        }
+
+        return String.format("%s/%s?key=%s&secret=%s", API, groupIdentity, CONSUMER_KEY, CONSUMER_SECRET);
+    }
+
+    private static String getGroupIdFromResponse(JSONObject response) {
+        try {
+            JSONArray results = response.getJSONArray("results");
+            JSONObject result = results.getJSONObject(0);
+            return String.valueOf(result.getInt("id"));
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    private static Map<String, Set<String>> getGroups(final Map<String, String> artists) throws IOException {
+        final Map<String, Set<String>> groups = new TreeMap<>(Collator.getInstance(new Locale("pl", "PL")));
+        for (String artist : artists.keySet()) {
+            String response = getResponse(artists.get(artist));
+            if (response == null) {
+                return null;
+            }
+
+            JSONObject jsonResponse = new JSONObject(response);
+            List<String> groupsFromResponse = getGroupsFromResponse(jsonResponse);
+            if (groupsFromResponse == null) {
+                return null;
+            }
+
+            for (String group : groupsFromResponse) {
+                if (groups.containsKey(group)) {
+                    groups.get(group).add(artist);
+                } else {
+                    Set<String> groupMembers = new HashSet<>();
+                    groupMembers.add(artist);
+                    groups.put(group, groupMembers);
+                }
+            }
+        }
+
+        return groups;
+    }
+
+    private static List<String> getGroupsFromResponse(final JSONObject response) {
+        List<String> groups = new ArrayList<>();
+
+        try {
+            JSONArray results = response.getJSONArray("groups");
+            for (Object result : results) {
+                JSONObject group = (JSONObject) result;
+                String groupName = group.getString("name");
+                groups.add(groupName);
+            }
+        } catch (JSONException e) {
+            return null;
+        }
+
+        return groups;
+    }
+
+    private static Map<String, String> getArtistsFromResponse(final JSONObject response) {
+        final Map<String, String> artists = new HashMap<>();
+
+        try {
+            JSONArray results = response.getJSONArray("members");
+            for (Object result : results) {
+                JSONObject artist = (JSONObject) result;
+                String artistName = artist.getString("name");
+                String artistUrl = artist.getString("resource_url");
+                artists.put(artistName, artistUrl);
+            }
+        } catch (JSONException e) {
+            return null;
+        }
+
+        return artists;
+    }
+
+    private static String getResponse(final String url) throws IOException {
+        final URL connectionUrl = new URL(url);
+        final HttpURLConnection connection = (HttpURLConnection) connectionUrl.openConnection();
+        connection.setConnectTimeout(5000);
+
+        if (connection.getResponseCode() != 200) {
+            return null;
+        }
+        if (!connection.getContentType().contains("application/json")) {
+            return null;
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String responseContent = getResponseContent(reader);
+
+        connection.disconnect();
+        reader.close();
+        return responseContent;
+    }
+
+    private static String getResponseContent(final BufferedReader reader) throws IOException {
+        final StringBuilder responseContent = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            responseContent.append(line);
+        }
+        return responseContent.toString();
+    }
+}
